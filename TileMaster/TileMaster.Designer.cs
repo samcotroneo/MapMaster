ab1ee7b83b6e77eb8aff5c6f6f4213e72bcd50e5
Sam Cotroneo <sammy.cotraz@gmail.com>
2020-03-24 19:51:38 +1100

    Initial commit for the refreshed MapMaster repo

---
+++ TileMaster/TileMaster.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace TileMaster
+{
+    partial class TileMaster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FolderPathTBox = new System.Windows.Forms.TextBox();
+            this.ImgPathTBox = new System.Windows.Forms.TextBox();
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ColorPickButton = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ImagePathPicker = new System.Windows.Forms.Button();
+            this.OutputPathPicker = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.ImageError = new System.Windows.Forms.Label();
+            this.OutputError = new System.Windows.Forms.Label();
+            this.ProcessingError = new System.Windows.Forms.Label();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.ProcessingLabel = new System.Windows.Forms.Label();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FolderPathTBox
+            // 
+            this.FolderPathTBox.Location = new System.Drawing.Point(12, 91);
+            this.FolderPathTBox.Name = "FolderPathTBox";
+            this.FolderPathTBox.Size = new System.Drawing.Size(360, 20);
+            this.FolderPathTBox.TabIndex = 1;
+            // 
+            // ImgPathTBox
+            // 
+            this.ImgPathTBox.Location = new System.Drawing.Point(12, 36);
+            this.ImgPathTBox.Name = "ImgPathTBox";
+            this.ImgPathTBox.Size = new System.Drawing.Size(360, 20);
+            this.ImgPathTBox.TabIndex = 2;
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(333, 128);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(75, 44);
+            this.ProcessButton.TabIndex = 3;
+            this.ProcessButton.Text = "Process Image";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Image Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tile Output Folder:";
+            // 
+            // ColorPickButton
+            // 
+            this.ColorPickButton.BackColor = System.Drawing.Color.White;
+            this.ColorPickButton.Location = new System.Drawing.Point(12, 144);
+            this.ColorPickButton.Name = "ColorPickButton";
+            this.ColorPickButton.Size = new System.Drawing.Size(23, 23);
+            this.ColorPickButton.TabIndex = 6;
+            this.ColorPickButton.UseVisualStyleBackColor = false;
+            this.ColorPickButton.Click += new System.EventHandler(this.ColorPickButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Background Colour:";
+            // 
+            // ImagePathPicker
+            // 
+            this.ImagePathPicker.Location = new System.Drawing.Point(383, 35);
+            this.ImagePathPicker.Name = "ImagePathPicker";
+            this.ImagePathPicker.Size = new System.Drawing.Size(25, 20);
+            this.ImagePathPicker.TabIndex = 8;
+            this.ImagePathPicker.Text = "...";
+            this.ImagePathPicker.UseVisualStyleBackColor = true;
+            this.ImagePathPicker.Click += new System.EventHandler(this.ImagePathPicker_Click);
+            // 
+            // OutputPathPicker
+            // 
+            this.OutputPathPicker.Location = new System.Drawing.Point(383, 91);
+            this.OutputPathPicker.Name = "OutputPathPicker";
+            this.OutputPathPicker.Size = new System.Drawing.Size(25, 20);
+            this.OutputPathPicker.TabIndex = 9;
+            this.OutputPathPicker.Text = "...";
+            this.OutputPathPicker.UseVisualStyleBackColor = true;
+            this.OutputPathPicker.Click += new System.EventHandler(this.OutputPathPicker_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // ImageError
+            // 
+            this.ImageError.AutoSize = true;
+            this.ImageError.ForeColor = System.Drawing.Color.Red;
+            this.ImageError.Location = new System.Drawing.Point(12, 56);
+            this.ImageError.Name = "ImageError";
+            this.ImageError.Size = new System.Drawing.Size(81, 13);
+            this.ImageError.TabIndex = 10;
+            this.ImageError.Text = "Invalid file path.";
+            this.ImageError.Visible = false;
+            // 
+            // OutputError
+            // 
+            this.OutputError.AutoSize = true;
+            this.OutputError.ForeColor = System.Drawing.Color.Red;
+            this.OutputError.Location = new System.Drawing.Point(9, 111);
+            this.OutputError.Name = "OutputError";
+            this.OutputError.Size = new System.Drawing.Size(94, 13);
+            this.OutputError.TabIndex = 11;
+            this.OutputError.Text = "Invalid folder path.";
+            this.OutputError.Visible = false;
+            // 
+            // ProcessingError
+            // 
+            this.ProcessingError.AutoSize = true;
+            this.ProcessingError.ForeColor = System.Drawing.Color.Red;
+            this.ProcessingError.Location = new System.Drawing.Point(278, 144);
+            this.ProcessingError.Name = "ProcessingError";
+            this.ProcessingError.Size = new System.Drawing.Size(49, 13);
+            this.ProcessingError.TabIndex = 12;
+            this.ProcessingError.Text = "ERROR!";
+            this.ProcessingError.Visible = false;
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(12, 214);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(396, 23);
+            this.ProgressBar.TabIndex = 13;
+            this.ProgressBar.Visible = false;
+            // 
+            // ProcessingLabel
+            // 
+            this.ProcessingLabel.AutoSize = true;
+            this.ProcessingLabel.Location = new System.Drawing.Point(12, 195);
+            this.ProcessingLabel.Name = "ProcessingLabel";
+            this.ProcessingLabel.Size = new System.Drawing.Size(68, 13);
+            this.ProcessingLabel.TabIndex = 14;
+            this.ProcessingLabel.Text = "Processing...";
+            this.ProcessingLabel.Visible = false;
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ProgressLabel.AutoSize = true;
+            this.ProgressLabel.Location = new System.Drawing.Point(201, 219);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(0, 13);
+            this.ProgressLabel.TabIndex = 15;
+            this.ProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ProgressLabel.Visible = false;
+            // 
+            // TileMaster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 249);
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.ProcessingLabel);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.ProcessingError);
+            this.Controls.Add(this.OutputError);
+            this.Controls.Add(this.ImageError);
+            this.Controls.Add(this.OutputPathPicker);
+            this.Controls.Add(this.ImagePathPicker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ColorPickButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProcessButton);
+            this.Controls.Add(this.ImgPathTBox);
+            this.Controls.Add(this.FolderPathTBox);
+            this.Name = "TileMaster";
+            this.Text = "Tile Master";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox FolderPathTBox;
+        private System.Windows.Forms.TextBox ImgPathTBox;
+        private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ColorPickButton;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ImagePathPicker;
+        private System.Windows.Forms.Button OutputPathPicker;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label OutputError;
+        private System.Windows.Forms.Label ImageError;
+        private System.Windows.Forms.Label ProcessingError;
+        private System.Windows.Forms.Label ProgressLabel;
+        private System.Windows.Forms.Label ProcessingLabel;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+    }
+}
+
---
+++ TileMaster/TileMaster.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TileMaster
+{
+    public partial class TileMaster : Form
+    {
+        public TileMaster()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //focus first text box
+            ActiveControl = ImgPathTBox;
+        }
+
+        #region Variables
+
+        //size for output image tiles
+        private readonly int standardTileSize = 256;
+
+        //layers of zoom
+        private int zoom;
+
+        //image to tile
+        private Bitmap src;
+
+        //orientation of image
+        private bool landscape;
+
+        //ratio between width and height
+        private int ratio;
+
+        #endregion
+
+        #region UI Handling
+
+        #region Interactions
+
+        private async void ProcessButton_Click(object sender, EventArgs e)
+        {
+            //make sure paths exist
+            if (!Directory.Exists(ImgPathTBox.Text.Substring(0, ImgPathTBox.Text.LastIndexOf("\\"))))
+            {
+                ImageError.Visible = true;
+                return;
+            }
+
+            ImageError.Visible = false;
+
+            if (!Directory.Exists(FolderPathTBox.Text))
+            {
+                OutputError.Visible = true;
+                return;
+            }
+
+            OutputError.Visible = false;
+
+            ProcessingError.Visible = await Task.Run(() => ProcessSequence());
+        }
+
+        private bool ProcessSequence()
+        {
+            //disable UI
+            UpdateProgressBar(0f);
+            ToggleUI(false);
+
+            //process that image
+            if (!LoadImage())
+            {
+                ToggleUI(true);
+                return true;
+            }
+                
+
+            if (!CreateFolders())
+            {
+                ToggleUI(true);
+                return true;
+            }
+
+            if (!ProcessImage())
+            {
+                ToggleUI(true);
+                return true;
+            }
+
+            ToggleUI(true);
+            return false;
+        }
+
+        private void ColorPickButton_Click(object sender, EventArgs e)
+        {
+            //pick colour of background of image
+            if (colorDialog.ShowDialog() == DialogResult.OK) ColorPickButton.BackColor = colorDialog.Color;
+        }
+
+        private void ToggleUI(bool state)
+        {
+            //disable or enable UI before or after processing image
+            Invoke((MethodInvoker)delegate {
+                //running on the UI thread
+                ProcessButton.Enabled = state;
+                FolderPathTBox.Enabled = state;
+                ImgPathTBox.Enabled = state;
+                OutputPathPicker.Enabled = state;
+                ImagePathPicker.Enabled = state;
+                ColorPickButton.Enabled = state;
+
+                ProcessingLabel.Visible = !state;
+                ProgressBar.Visible = !state;
+                ProgressLabel.Visible = !state;
+
+                ProgressLabel.BringToFront();
+            });
+        }
+
+        private void UpdateProgressBar(float percentage)
+        {
+            //update visual progress of image processing
+            Invoke((MethodInvoker)delegate {
+                //running on the UI thread
+                ProgressLabel.Text = Math.Round((double)percentage, 2, MidpointRounding.AwayFromZero) + "%";
+                ProgressBar.Value = (int)Math.Floor(percentage);
+            });
+        }
+        #endregion
+
+        #region File Folder Explorer
+
+        //pick image file
+        private void ImagePathPicker_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+            OpenFileDialog fileBrowserDialog = new OpenFileDialog();
+            if (fileBrowserDialog.ShowDialog() == DialogResult.OK) filePath = fileBrowserDialog.FileName;
+
+            ImgPathTBox.Text = filePath;
+        }
+
+        //pick output path
+        private void OutputPathPicker_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK) folderPath = folderBrowserDialog.SelectedPath;
+
+            FolderPathTBox.Text = folderPath;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Create Folder Structure
+
+        private bool CreateFolders()
+        {
+            try
+            {
+                //create folders for each zoom layer
+                string pathString = FolderPathTBox.Text;
+
+                for (int i = 0; i < zoom; i++)
+                {
+                    string subFolder = "" + i;
+                    int numOfFolders = 0;
+                    if (landscape)
+                        numOfFolders = (int)Math.Pow(2, i);
+                    else
+                        numOfFolders = (int)Math.Ceiling(Math.Pow(2, i) / ratio);
+                    if(!CreateSubFolders(pathString, numOfFolders, subFolder))
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CreateSubFolders(string folderPath, int numberOfFolders, string subFolderName)
+        {
+            try
+            {
+                //create sub folders in zoom folders for each column of tiles
+                string newFolder = folderPath + "\\" + subFolderName;
+                Directory.CreateDirectory(newFolder);
+
+                for (int i = 0; i < numberOfFolders; i++)
+                {
+                    string newerFolder = newFolder + "\\" + i;
+                    Directory.CreateDirectory(newerFolder);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Image Math
+
+        private bool LoadImage()
+        {
+            try
+            {
+                //load default image
+                string fileName = ImgPathTBox.Text;
+                src = Image.FromFile(fileName) as Bitmap;
+
+                //get orientation of image
+                landscape = false;
+                if (src.Width - src.Height > 0) landscape = true;
+
+                //determine zoom
+                bool done = false;
+                int maxColumns = 0;
+                int newWidth = 0;
+                for (int i = 0; !done; i++)
+                {
+                    int check = (int) (standardTileSize * Math.Pow(2, i));
+                    if (check >= src.Width)
+                    {
+                        newWidth = check;
+                        maxColumns = (int) Math.Pow(2, i);
+                        done = true;
+                        zoom = i + 1;
+                    }
+                }
+
+                done = false;
+                int maxRows = 0;
+                int newHeight = 0;
+                for (int i = 0; !done; i++)
+                {
+                    int check = (int) (standardTileSize * Math.Pow(2, i));
+                    if (check >= src.Height)
+                    {
+                        newHeight = check;
+                        maxRows = (int) Math.Pow(2, i);
+                        done = true;
+                        if (i + 1 > zoom) zoom = i + 1;
+                    }
+                }
+
+                //resize image if needed for nice tiling
+                if (src.Width != newWidth || src.Height != newHeight)
+                    src = ChangeDimensions(src, newWidth, newHeight, true, ColorPickButton.BackColor);
+
+                //determine ratio between height and width
+                ratio = src.Height / src.Width;
+                if (landscape) ratio = src.Width / src.Height;
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ProcessImage()
+        {
+            try
+            {
+                //progress calc
+                int currentProcess = 0;
+                int numberOfProcess = 0;
+                for(int c = 0; c < zoom; c++)
+                    numberOfProcess += (int)Math.Pow(2, c) * (int)Math.Ceiling(Math.Pow(2, c) / ratio);
+
+                //loop through zoom layers
+                for (int i = 0; i < zoom; i++)
+                {
+                    //get number of columns of tiles in current zoom layer
+                    int loops = 0;
+                    if (landscape)
+                        loops = (int)Math.Pow(2, i);
+                    else
+                        loops = (int)Math.Ceiling(Math.Pow(2, i) / ratio);
+                    //loop through columns
+                    for (int j = 0; j < loops; j++)
+                    {
+                        //get number of tiles in column for zoom layer
+                        int halfLoops = 0;
+                        if (landscape)
+                            halfLoops = (int)Math.Ceiling(Math.Pow(2, i) / ratio);
+                        else
+                            halfLoops = (int)Math.Pow(2, i);
+                        //loop through tiles
+                        for (int k = 0; k < halfLoops; k++)
+                        {
+                            //determine output folder (created by CreateFolders)
+                            string savePath = FolderPathTBox.Text + "\\" + i + "\\" + j + "\\" + k + ".png";
+                            //special case for squaring the most zoomed out version of base image (1 tile in - square shaped)
+                            if (i == 0)
+                            {
+                                //resize original image and make it square
+                                if (landscape)
+                                    ChangeDimensions(ResizeImage(src, standardTileSize, standardTileSize / ratio),
+                                            standardTileSize, standardTileSize, false, ColorPickButton.BackColor)
+                                        .Save(savePath);
+                                else
+                                    ChangeDimensions(ResizeImage(src, standardTileSize / ratio, standardTileSize),
+                                            standardTileSize, standardTileSize, false, ColorPickButton.BackColor)
+                                        .Save(savePath);
+
+                                break;
+                            }
+
+                            //get size of tile to cut from base image and create a rect to reflect that
+                            int size = 256 * (int)Math.Pow(2, zoom - i - 1);
+                            Rectangle cropRect = new Rectangle(size * j, size * k, size, size);
+
+                            //create a new bitmap image and draw the a tile from the original image of the size and position of the rest created
+                            using (Bitmap target = new Bitmap(cropRect.Width, cropRect.Height))
+                            {
+                                using (Graphics g = Graphics.FromImage(target))
+                                {
+                                    g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height),
+                                        cropRect,
+                                        GraphicsUnit.Pixel);
+                                }
+
+                                //save tile to the folder created earlier
+                                ResizeImage(target, standardTileSize, standardTileSize).Save(savePath);
+                            }
+
+                            //update progress bar
+                            currentProcess++;
+                            UpdateProgressBar(((float)currentProcess / (float)numberOfProcess) * 100);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Image Manipulation
+
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            //create new bitmap to draw to
+            Rectangle destRect = new Rectangle(0, 0, width, height);
+            Bitmap destImg = new Bitmap(width, height);
+            destImg.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            //draw to new bitmap - resize using high quality settings
+            using (Graphics g = Graphics.FromImage(destImg))
+            {
+                g.CompositingMode = CompositingMode.SourceCopy;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (ImageAttributes wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    g.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImg;
+        }
+
+        private static Bitmap ChangeDimensions(Image image, int width, int height, bool centered, Color backColor)
+        {
+            //create new bitmap to draw to
+            Bitmap destImg = new Bitmap(width, height);
+            Rectangle destRect;
+            if (centered)
+                destRect = new Rectangle((width - image.Width) / 2, (height - image.Height) / 2, image.Width,
+                    image.Height);
+            else
+                destRect = new Rectangle(0, 0, image.Width, image.Height);
+
+            //draw to new bitmap and fill extra space with a colour
+            using (Graphics g = Graphics.FromImage(destImg))
+            {
+                g.Clear(backColor);
+                using (ImageAttributes wrapMode = new ImageAttributes())
+                {
+                    g.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImg;
+        }
+
+        #endregion
+    }
+}
